73c1d85bcbc75c71c63df4b04a2198e4631f268e
dongphong543 <dongphong543@gmail.com>
2021-07-15 22:01:46 +0700

    Temp signup screen

--- App.js
+++ App.js
@@ -1,148 +1,28 @@
-import { StatusBar } from "expo-status-bar";
-import React, {useState} from "react";
-import { StyleSheet, Text, View, Image, Dimensions, Alert } from "react-native";
-import GradientButton from 'react-native-gradient-buttons';
-import * as Font from "expo-font";
-import { AppLoading } from 'expo';
+import React from "react";
 
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-const windowWidth = Dimensions.get('window').width;
-const windowHeight = Dimensions.get('window').height;
-
-const showAlert = (alertTitle, alertText) =>
-  Alert.alert(
-    alertTitle,
-    alertText,
-    [
-      {
-        text: "OK",
-        //onPress: () => Alert.alert("OK Pressed"),
-        style: "cancel",
-      },
-    ],
-    {
-      cancelable: true,
-      /* onDismiss: () =>
-        Alert.alert(
-          "This alert was dismissed by tapping outside of the alert dialog."
-        ), */
-    }
-  );
-
-/*
-const fetchFonts = () => {
-  return Font.loadAsync({
-    Bahn: require('./assets/bahnschrift.ttf')
-  });
-  };
-*/
-
+import FirstScreen from "./Navigations/firstscreen";
+import SecondScreen from "./Navigations/signupscreen";
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-/*  const [dataLoaded, setDataLoaded] = useState(false);
-
-  if (!dataLoaded) {
-    return(
-      <AppLoading
-        startAsync={fetchFonts}
-        onFinish={() => setDataLoaded(true)} 
-      />
-    );
-  }
-*/
   return (
-    
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Image style={{
-        
-        width: windowWidth/1.5 >= 350? 350 : windowWidth/1.5,
-        height: windowHeight/4,
-        resizeMode: 'contain',
-      }}
-        source={require("./assets/Routines_logo.png")}
-      />
-      
-      <GradientButton
-        text="USING THIS DEVICE ONLY"
-        textStyle={{ fontSize: 18 }}
-        style={{
-          marginVertical: 10,
-          shadowColor: "#000",
-          shadowOffset: {
-            width: 0,
-            height: 2,
-          },
-          shadowOpacity: 0.2,
-          shadowRadius: 3,
-          
-          elevation: 3,
-          borderRadius: 7 }}
-        gradientBegin="#0359e3"
-        gradientEnd="#0041b1"
-        gradientDirection="radial"
-        height={45}
-        width={windowWidth/1.5 >= 350? 350 : windowWidth/1.5}
-        radius={7}
-        impact
-        impactStyle='Light'
-        onPressAction={() => showAlert('Device only', 'In developing')}
-      />
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="FirstScreen">
+        <Stack.Screen
+          name="FirstScreen"
+          component={FirstScreen}
+          options={{ headerShown: false }}
+        />
 
-      <GradientButton
-        text="LOG IN AND SYNC 🔒"
-        textStyle={{ fontSize: 18, color: '#bbb' }}
-        style={{
-          shadowColor: "#000",
-          shadowOffset: {
-            width: 0,
-            height: 2,
-          },
-          shadowOpacity: 0.2,
-          shadowRadius: 3,
-          
-          elevation: 3,
-          borderRadius: 7 }}
-        gradientBegin="#ddd"
-        gradientEnd="#ddd"
-        gradientDirection="radial"
-        height={45}
-        width={windowWidth/1.5 >= 350? 350 : windowWidth/1.5}
-        radius={7}
-        impact
-        impactStyle='Light'
-        onPressAction={() => showAlert('Log in and Sync','Unavailable feature!')}
-      />
-
-      <GradientButton
-        text="Not having an account? Sign up"
-        textStyle={{ fontSize: 14, color: '#0359e3' }}
-        style={{marginVertical: 50}}
-        gradientBegin={styles.container.backgroundColor}
-        gradientEnd={styles.container.backgroundColor}
-        gradientDirection="radial"
-        height={40}
-        width={windowWidth/1.6 >= 350? 350 : windowWidth/1.6}
-        radius={7}
-        impact
-        impactStyle='Light'
-        onPressAction={() => showAlert('Sign up','Unavailable feature!')}
-      />      
-
-    </View>
-    
+        <Stack.Screen
+          name="SecondScreen"
+          component={SecondScreen}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    justifyContent: "center",
-    alignItems: "center"
-  },
-
-});
-
-
---
+++ Navigations/firstscreen.js
@@ -0,0 +1,145 @@
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Image, Dimensions, Alert } from "react-native";
+import GradientButton from "react-native-gradient-buttons";
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
+
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
+
+const showAlert = (alertTitle, alertText) =>
+  Alert.alert(
+    alertTitle,
+    alertText,
+    [
+      {
+        text: "OK",
+        //onPress: () => Alert.alert("OK Pressed"),
+        style: "cancel",
+      },
+    ],
+    {
+      cancelable: true,
+      /* onDismiss: () =>
+        Alert.alert(
+          "This alert was dismissed by tapping outside of the alert dialog."
+        ), */
+    }
+  );
+
+/*
+const fetchFonts = () => {
+  return Font.loadAsync({
+    Bahn: require('./assets/bahnschrift.ttf')
+  });
+  };
+*/
+
+const FirstScreen = ({ navigation }) => {
+  /*  const [dataLoaded, setDataLoaded] = useState(false);
+
+  if (!dataLoaded) {
+    return(
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setDataLoaded(true)} 
+      />
+    );
+  }
+*/
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <Image
+        style={{
+          width: windowWidth / 1.5 >= 350 ? 350 : windowWidth / 1.5,
+          height: windowHeight / 4,
+          resizeMode: "contain",
+        }}
+        source={require("../assets/Routines_logo.png")}
+      />
+
+      <GradientButton
+        text="USING THIS DEVICE ONLY"
+        textStyle={{ fontSize: 18 }}
+        style={{
+          marginVertical: 10,
+          shadowColor: "#000",
+          shadowOffset: {
+            width: 0,
+            height: 2,
+          },
+          shadowOpacity: 0.2,
+          shadowRadius: 3,
+
+          elevation: 3,
+          borderRadius: 7,
+        }}
+        gradientBegin="#0359e3"
+        gradientEnd="#0041b1"
+        gradientDirection="radial"
+        height={45}
+        width={windowWidth / 1.5 >= 350 ? 350 : windowWidth / 1.5}
+        radius={7}
+        impact
+        impactStyle="Light"
+        onPressAction={() => navigation.navigate("SecondScreen")} //showAlert("Device only", "In developing")}
+      />
+
+      <GradientButton
+        text="LOG IN AND SYNC 🔒"
+        textStyle={{ fontSize: 18, color: "#bbb" }}
+        style={{
+          shadowColor: "#000",
+          shadowOffset: {
+            width: 0,
+            height: 2,
+          },
+          shadowOpacity: 0.2,
+          shadowRadius: 3,
+
+          elevation: 3,
+          borderRadius: 7,
+        }}
+        gradientBegin="#ddd"
+        gradientEnd="#ddd"
+        gradientDirection="radial"
+        height={45}
+        width={windowWidth / 1.5 >= 350 ? 350 : windowWidth / 1.5}
+        radius={7}
+        impact
+        impactStyle="Light"
+        onPressAction={() =>
+          showAlert("Log in and Sync", "Unavailable feature!")
+        }
+      />
+
+      <GradientButton
+        text="Not having an account? Sign up"
+        textStyle={{ fontSize: 14, color: "#0359e3" }}
+        style={{ marginVertical: 50 }}
+        gradientBegin={styles.container.backgroundColor}
+        gradientEnd={styles.container.backgroundColor}
+        gradientDirection="radial"
+        height={40}
+        width={windowWidth / 1.6 >= 350 ? 350 : windowWidth / 1.6}
+        radius={7}
+        impact
+        impactStyle="Light"
+        onPressAction={() => showAlert("Sign up", "Unavailable feature!")}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default FirstScreen;
