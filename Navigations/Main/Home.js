c58edc758b5bff6752cbe5cfc49f3b53dabf61f8
Quan Nguyen <minhquan0902@gmail.com>
2021-07-22 01:53:35 +0800

    Updated Firestore realtime Data check

--- Navigations/Main/Home.js
+++ Navigations/Main/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Text, View, Dimensions, Button } from "react-native";
 import GradientButton from "react-native-gradient-buttons";
 
@@ -6,36 +6,53 @@ const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
 import { firebase } from "../../firebase/config";
 
-const LogOut = () => {
-  firebase.auth().signOut();
-};
-const userId = firebase.auth().currentUser.uid;
+// Check for current user
 
-// const userId = firebase.auth().currentUser.uid;
-const db = firebase.firestore();
-
-// Still on testing
-
-db.collection("users")
-  .doc(userId)
-  .get()
-  .then((snapshot) => {
-    if (snapshot.exist) {
-      console.log("Document data: ", snapshot.data());
-    } else {
-      //snapshot.data() will be undefined this time
-      console.log("No such document!");
-    }
-  })
-  .catch((error) => {
-    console.log("Error getting document: ", error);
-  });
+// initialize DB
 
 export default function HomeScreen() {
+  const LogOut = () => {
+    firebase.auth().signOut();
+  };
+
+  const user = firebase.auth().currentUser;
+  const [username, setUsername] = useState("");
+  const [emailData, setEmailData] = useState("");
+  const uid = user.uid;
+  console.log("User UID: ", uid);
+  console.log("Current User: ", user);
+
+  useEffect(() => {
+    const usersRef = firebase.firestore().collection("users");
+    usersRef
+      .doc(uid)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          console.log("Document data:", doc.data());
+          let userData = doc.data();
+          console.log("UserData", userData.username);
+          setUsername(userData.username);
+          setEmailData(userData.email);
+        } else {
+          // doc.data() will be undefined in this case
+          console.log("No such document!");
+        }
+      })
+      .catch((error) => {
+        console.log("Error getting document:", error);
+      });
+  }, []);
+
   return (
     <View style={{ flex: 1, justifyContent: "center" }}>
       <Text style={{ textAlign: "center", marginBottom: 20 }}>Home Screen</Text>
-      <Text></Text>
+      <Text style={{ textAlign: "center", marginBottom: 20 }}>
+        Hello, {username}
+      </Text>
+      <Text style={{ textAlign: "center", marginBottom: 20 }}>
+        Your Email Address is: {emailData}
+      </Text>
       <Button
         style={{ marginTop: 20 }}
         onPress={() => LogOut()}
