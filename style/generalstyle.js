dcbd84080f2740d2c066dec72b5d228bf627f5e7
r4g3qu1t <thinh.lebkuhcm@hcmut.edu.vn>
2021-09-17 09:28:07 +0700

    style and theme usually used

---
+++ style/generalstyle.js
@@ -0,0 +1,32 @@
+const focusStyle = {
+  height: 50,
+  backgroundColor: "#105cdd",
+  flex: 0.7,
+  shadowColor: "#000",
+  shadowOffset: {
+    width: 0,
+    height: 2,
+  },
+  shadowOpacity: 0.2,
+  shadowRadius: 3,
+  height: 50,
+
+  elevation: 3,
+  borderRadius: 5,
+};
+const blurStyle = {
+  height: 50,
+  backgroundColor: "#f8f8f8",
+  flex: 0.7,
+  shadowColor: "#000",
+  shadowOffset: {
+    width: 0,
+    height: 2,
+  },
+  shadowOpacity: 0.2,
+  shadowRadius: 3,
+
+  borderRadius: 5,
+  elevation: 3,
+};
+export { blurStyle, focusStyle };
