ca9f499a5bb6c2bb8f85ec4e901657856b975903
r4g3qu1t <thinh.lebkuhcm@hcmut.edu.vn>
2022-01-01 22:53:11 +0700

    add white type Button and password type TextInput

--- Navigations/TextInput.js
+++ Navigations/TextInput.js
@@ -2,10 +2,10 @@ import { useState } from "react";
 import { TextInput } from "react-native-paper";
 import * as styles from "../style/generalstyle";
 import * as themes from "../style/generaltheme";
-export function DefautlTextInput(props) {
+function DefautlTextInput(props) {
   const [isFocus, setFocus] = useState(false);
   const [value, setValue] = useState("");
-  return (
+  return (props.type = "Text" ? (
     <TextInput
       placeholderTextColor={
         props.placeholderTextColor ? props.placeholderTextColor : "#aab"
@@ -20,27 +20,41 @@ export function DefautlTextInput(props) {
       underlineColor={props.underlineColor ? props.underlineColor : "fff"}
       selectionColor={props.underlineColor ? props.underlineColor : "abf"}
     ></TextInput>
-  );
+  ) : (
+    (props.type = "Number" ? (
+      <TextInput
+        placeholderTextColor={
+          props.placeholderTextColor ? props.placeholderTextColor : "#aab"
+        }
+        style={isFocus ? styles.focusStyle : styles.blurStyle}
+        theme={isFocus ? themes.focusTheme : themes.blurTheme}
+        onFocus={() => setFocus(true)}
+        onBlur={() => setFocus(false)}
+        placeholder={props.placeholder}
+        underlineColor={props.underlineColor ? props.underlineColor : "fff"}
+        selectionColor={props.underlineColor ? props.underlineColor : "abf"}
+        onChangeText={(value) => setValue(value)}
+        value={value}
+        keyboardType={"number-pad"}
+        textAlign={props.textAlign ? props.textAlign : "center"}
+      ></TextInput>
+    ) : (
+      <TextInput
+        placeholderTextColor={
+          props.placeholderTextColor ? props.placeholderTextColor : "#aab"
+        }
+        style={isFocus ? styles.focusStyle : styles.blurStyle}
+        theme={isFocus ? themes.focusTheme : themes.blurTheme}
+        onChangeText={(value) => setValue(value)}
+        onFocus={() => setFocus(true)}
+        onBlur={() => setFocus(false)}
+        placeholder={props.placeholder}
+        value={value}
+        underlineColor={props.underlineColor ? props.underlineColor : "fff"}
+        selectionColor={props.underlineColor ? props.underlineColor : "abf"}
+        secureTextEntry={true}
+      ></TextInput>
+    ))
+  ));
 }
-export function DefautlNumberInput(props) {
-  const [isFocus, setFocus] = useState(false);
-  const [value, setValue] = useState("");
-  return (
-    <TextInput
-      placeholderTextColor={
-        props.placeholderTextColor ? props.placeholderTextColor : "#aab"
-      }
-      style={isFocus ? styles.focusStyle : styles.blurStyle}
-      theme={isFocus ? themes.focusTheme : themes.blurTheme}
-      onFocus={() => setFocus(true)}
-      onBlur={() => setFocus(false)}
-      placeholder={props.placeholder}
-      underlineColor={props.underlineColor ? props.underlineColor : "fff"}
-      selectionColor={props.underlineColor ? props.underlineColor : "abf"}
-      onChangeText={(value) => setValue(value)}
-      value={value}
-      keyboardType={"number-pad"}
-      textAlign={props.textAlign ? props.textAlign : "center"}
-    ></TextInput>
-  );
-}
+export { DefautlTextInput };
